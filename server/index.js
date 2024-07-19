8b989c1e64156219a79cdc3c783d03ff36bcafa8
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-07-19 18:01:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ console.log("IO PORT=",PORT);
 Connection();
 
 const corsOptions = {
-    origin: 'http://localhost:3001', // Allow only your frontend's origin
+    origin: 'https://co-create-g245joz1l-vishesh-guptas-projects.vercel.app/', // Allow only your frontend's origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // Allow cookies to be sent
     optionsSuccessStatus: 204
