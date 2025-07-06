1e1b227f7e9ee94e43808110d178d69ffdc8e21f
Vishesh <vishesh15th@gmail.com>
2025-07-07 01:04:03 +0530

    Make some firebase env changes in the frontend

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,15 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAiSs6Ckt7UZkmNvwJXYqshKfyDj3zmXjs",
-  authDomain: "docs-clone-2447d.firebaseapp.com",
-  projectId: "docs-clone-2447d",
-  storageBucket: "docs-clone-2447d.appspot.com",
-  messagingSenderId: "1013362966586",
-  appId: "1:1013362966586:web:642f68ec6392e1caa67503"
-};
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
